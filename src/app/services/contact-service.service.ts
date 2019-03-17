a3110e9686252cf51d33d7226374ddc93396e325
Bryan Astacio <brya0196@gmail.com>
2019-03-16 20:46:40 -0400

    getting data from the contact-api.json

--- src/app/services/contact-service.service.ts
+++ src/app/services/contact-service.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IContact } from '../interfaces/icontact';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactServiceService {
+  contacts: Array<IContact>;
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.getContact().subscribe(data => {
+      console.log(data);
+      this.contacts = data;
+    });
+  }
+
+  public getContact(): Observable<any> {
+    return this.http.get("./contact-api.json");
+  }
 }
